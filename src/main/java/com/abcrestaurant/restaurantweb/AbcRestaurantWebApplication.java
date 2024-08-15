718c355f9e95834c6a237dda3be9d03747458283
Umindu Chethiya <91871991+Nipuniii@users.noreply.github.com>
2024-08-15 07:31:45 +0530

    initial commite to develop

---
+++ src/main/java/com/abcrestaurant/restaurantweb/AbcRestaurantWebApplication.java
@@ -0,0 +1,15 @@
+package com.abcrestaurant.restaurantweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcRestaurantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcRestaurantWebApplication.class, args);
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ src/test/java/com/abcrestaurant/restaurantweb/AbcRestaurantWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcrestaurant.restaurantweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcRestaurantWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

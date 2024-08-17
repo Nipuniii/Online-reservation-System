4b777f2888cf731479009ca33088cbe8f7aad581
Umindu Chethiya <91871991+Nipuniii@users.noreply.github.com>
2024-08-17 17:57:51 +0530

    about and contact us ui done
    about and contact us ui done

--- src/main/java/com/abcrestaurant/restaurantweb/controller/UserController.java
+++ src/main/java/com/abcrestaurant/restaurantweb/controller/UserController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
     @GetMapping("/home")
-    public String showLoginPage() {
+    public String showHomePage() {
         return "user/home"; // Points to src/main/resources/templates/auth/login.html
+    }
+
+    @GetMapping("/contactus")
+    public String showContactPage() {
+        return "user/contactus"; // Points to src/main/resources/templates/auth/login.html
+    }
+
+    @GetMapping("/aboutus")
+    public String showAboutPage(){
+        return  "user/aboutus";
     }
 }

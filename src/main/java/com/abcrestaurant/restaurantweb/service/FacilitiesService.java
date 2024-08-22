92100b2c068b8f66d668a4719fa21623b28f617f
Umindu Chethiya <91871991+Nipuniii@users.noreply.github.com>
2024-08-22 10:14:35 +0530

    facilities add done
    facilities add done

---
+++ src/main/java/com/abcrestaurant/restaurantweb/service/FacilitiesService.java
@@ -0,0 +1,17 @@
+package com.abcrestaurant.restaurantweb.service;
+
+import com.abcrestaurant.restaurantweb.model.Facilities;
+import com.abcrestaurant.restaurantweb.repository.FacilitiesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FacilitiesService {
+
+    @Autowired
+    private FacilitiesRepository facilitiesRepository;
+
+    public Facilities saveFacilities(Facilities facilities) {
+        return facilitiesRepository.save(facilities);
+    }
+}

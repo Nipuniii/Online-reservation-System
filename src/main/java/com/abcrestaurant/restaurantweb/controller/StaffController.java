4dee87511e873908e61eef16aa365d1d04ea4155
Umindu Chethiya <91871991+Nipuniii@users.noreply.github.com>
2024-08-26 14:43:31 +0530

    staff panel done
    staff panel done

--- src/main/java/com/abcrestaurant/restaurantweb/controller/StaffController.java
+++ src/main/java/com/abcrestaurant/restaurantweb/controller/StaffController.java
@@ -1,5 +1,13 @@
 package com.abcrestaurant.restaurantweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class StaffController {
+    @GetMapping("/staffdashboard")
+    public String viewstaffdashboard() {
+        return "staff/staffdashboard"; // Renders admin-dashboard.html
+    }
+
 
 }
